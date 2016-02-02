a544af1764ba446d810605603bc0bc2ffae88f43
SimpleSol <gworkmaill@gmail.com>
2016-02-02 19:23:54 +0300

    added toolbar for DetailsActivity

---
+++ app/src/main/java/com/example/leon/phimovies/ToDoClass.java
@@ -0,0 +1,22 @@
+package com.example.leon.phimovies;
+
+/**
+ * Created by Тихон on 02.02.2016.
+ */
+public class ToDoClass {
+
+    // TODO: 02.02.2016 (?) finish()
+
+    // TODO: 02.02.2016 Navigation Drawer over Toolbar
+    // TODO: 02.02.2016 Dynamic search
+    // TODO: 02.02.2016 ~ TabLayout within Toolbar
+    // TODO: 02.02.2016 Actual date in queries
+    // TODO: 02.02.2016 Reimplement UNDO within FavoriteFragment's SnackBar
+    // TODO: 02.02.2016 Inheritance
+
+    // TODO: 02.02.2016 Clean the code 
+
+
+    // TODO: 02.02.2016 Delete this Class 
+
+}
--- app/src/main/java/com/example/leon/phimovies/details_activity/DetailsActivity.java
+++ app/src/main/java/com/example/leon/phimovies/details_activity/DetailsActivity.java
@@ -2,7 +2,10 @@ package com.example.leon.phimovies.details_activity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.leon.phimovies.R;
@@ -29,11 +32,14 @@ public class DetailsActivity extends AppCompatActivity implements DetailsView {
     TextView mReleaseDate;
     @Bind(R.id.details_title)
     TextView mTitle;
+    @Bind(R.id.toolbar_details)
+    Toolbar mToolbar;
     private DetailsPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppDefault);
         setContentView(R.layout.ac_details);
 
         ButterKnife.bind(this);
@@ -43,8 +49,31 @@ public class DetailsActivity extends AppCompatActivity implements DetailsView {
         if (getIntent().getExtras() != null && getIntent().getExtras().containsKey(KEY_MOVIE)) {
             Bundle bundle = getIntent().getExtras();
             Movie movie = (Movie) bundle.getSerializable(KEY_MOVIE);
+            initToolbar(movie != null ? movie.getTitle() : getString(R.string.app_name));
             mPresenter.initMovieData(movie);
         }
+    }
+
+    private void initToolbar(String title) {
+        if (mToolbar != null) {
+            mToolbar.setTitle(title);
+            setSupportActionBar(mToolbar);
+            ActionBar supportActionBar = getSupportActionBar();
+            if (supportActionBar != null) {
+                supportActionBar.setDisplayHomeAsUpEnabled(true);
+                supportActionBar.setDisplayShowHomeEnabled(true);
+                supportActionBar.setHomeButtonEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @OnClick(R.id.button_add)
